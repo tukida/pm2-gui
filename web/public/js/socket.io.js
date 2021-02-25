82907ad62c428e186ed5d7bfd22072b6a55d2809
Kidataek <tuankiet.hcmc@gmail.com>
2021-02-25 21:48:05 +0700

    chore: update code

--- web/public/js/socket.io.js
+++ web/public/js/socket.io.js
@@ -131,7 +131,7 @@ function Manager(uri, opts){
   }
   opts = opts || {};
 
-  opts.path = opts.path || '/monitor/socket.io';
+  opts.path = opts.path || '/socket.io';
   this.nsps = {};
   this.subs = [];
   this.opts = opts;
